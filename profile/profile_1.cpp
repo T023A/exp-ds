c2bd1c174a1ad656466120f1f3ba333ac97a4749
Pawan <pawan.s.chawla@outlook.com>
2025-05-20 18:50:39 -0700

    Reformat and renaming

--- profile/profile_1.cpp
+++ profile/profile_1.cpp
@@ -1,17 +1,18 @@
-#include <iostream>
-#include <unordered_map_bit.hh>
-#include <unordered_set>
-#include <vector>
-#include <cstdlib>
-#include <unordered_map>
-#include <fstream>
-#include <random>
 #include <absl/container/flat_hash_set.h>
 
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <unordered_map>
+#include <unordered_set>
+#include <unordered_set_bit.hh>
+#include <vector>
+
 static inline uint64_t rdtsc() {
-    unsigned int lo,hi;
-    __asm__ __volatile__ ("rdtscp" : "=a" (lo), "=d" (hi));
-    return ((uint64_t)hi << 32) | lo;
+  unsigned int lo, hi;
+  __asm__ __volatile__("rdtscp" : "=a"(lo), "=d"(hi));
+  return ((uint64_t)hi << 32) | lo;
 }
 
 using namespace std;
@@ -20,303 +21,249 @@ std::vector<int64_t> ins_data;
 std::vector<int64_t> search_data;
 std::vector<int64_t> erase_data;
 
-
-typedef ds::ul_map<64,24> ul_map_t;
+typedef ds::ul_set<64, 24> ul_set_t;
 
 static int NUM_ELEMENTS = 3000000;
 
-
 void genData() {
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(123); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<int64_t> distrib(1, INT64_MAX);
+  std::random_device
+      rd;  // Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(123);  // Standard mersenne_twister_engine seeded with rd()
+  std::uniform_int_distribution<int64_t> distrib(1, INT64_MAX);
 
+  std::unordered_set<int64_t> s;
 
+  for (int i = 0; i < NUM_ELEMENTS; ++i) {
+    int64_t num = distrib(gen);
+    if (s.find(num) != s.end()) continue;
+    ins_data.push_back(num);
+    s.insert(num);
+  }
 
-    std::unordered_set<int64_t> s;
+  // shuffle search data
+  {
+    search_data = ins_data;
+    std::shuffle(search_data.begin(), search_data.end(), gen);
+  }
 
-    for (int i = 0; i < NUM_ELEMENTS; ++i) {
-        int64_t num = distrib(gen);
-        if (s.find(num) != s.end()) continue;
-        ins_data.push_back(num );
-        s.insert(num);
-
-    }
-
-
-    // shuffle search data
-    {
-        search_data = ins_data;
-        std::shuffle(search_data.begin(), search_data.end(), gen);
-    }
-
-    // shuffle erase data
-    {
-        erase_data = ins_data;
-        std::shuffle(erase_data.begin(), erase_data.end(), gen);
-    }
+  // shuffle erase data
+  {
+    erase_data = ins_data;
+    std::shuffle(erase_data.begin(), erase_data.end(), gen);
+  }
 }
 
 std::unordered_set<int64_t> us;
 int nh = 0, nm = 0, ni = 0, ne = 0;
 
 void test_stdmap_insert() {
-    for (int i = 0; i < NUM_ELEMENTS; ++i) {
-        int64_t v = ins_data[i], s = search_data[i];
+  for (int i = 0; i < NUM_ELEMENTS; ++i) {
+    int64_t v = ins_data[i], s = search_data[i];
 
-        us.insert(v);
-        ++ni;
-    }
+    us.insert(v);
+    ++ni;
+  }
 }
 
 void test_stdmap_search() {
-
-
-    for (int64_t s : search_data) {
-
-
-        bool ret = us.find(s) != us.end();
-        nh += ret;
-        nm += !ret;
-    }
-
-
+  for (int64_t s : search_data) {
+    bool ret = us.find(s) != us.end();
+    nh += ret;
+    nm += !ret;
+  }
 }
 
-
 void test_stdmap_erase() {
+  for (int i = 0; i < NUM_ELEMENTS; ++i) {
+    int64_t s = erase_data[i];
 
-    for (int i = 0; i < NUM_ELEMENTS; ++i) {
-        int64_t s = erase_data[i];
-
-
-        ne += us.erase(s);
-    }
-
-
+    ne += us.erase(s);
+  }
 }
 
 void test_stdmap_insert_search() {
+  for (int i = 0; i < NUM_ELEMENTS; ++i) {
+    int s = search_data[i];
+    int v = ins_data[i];
 
-
-
-
-    for (int i = 0; i < NUM_ELEMENTS; ++i) {
-        int s = search_data[i];
-        int v = ins_data[i];
-
-        ni +=  us.insert(v).second ;
-        bool ret = us.find(s) != us.end();
-        nh += ret;
-        nm += !ret;
-    }
-
-
+    ni += us.insert(v).second;
+    bool ret = us.find(s) != us.end();
+    nh += ret;
+    nm += !ret;
+  }
 }
 
-
-
-
-ul_map_t * ptr_um1 = nullptr;
-
-
+ul_set_t* ptr_um1 = nullptr;
 
 void test_dsmap_insert() {
+  auto& um1 = *ptr_um1;
 
-    auto& um1 = *ptr_um1;
+  for (int i = 0; i < NUM_ELEMENTS; ++i) {
+    int64_t v = ins_data[i];
 
-    for (int i = 0; i < NUM_ELEMENTS; ++i) {
-        int64_t v = ins_data[i];
+    um1.insert(v);
 
-
-        um1.insert(v);
-
-        ++ni;
-
-    }
-
-
+    ++ni;
+  }
 }
 
 void test_dsmap_find() {
+  auto& um1 = *ptr_um1;
 
-    auto& um1 = *ptr_um1;
-
-    for (int64_t s : search_data) {
-
-
-        int ret = um1.exists(s);
-        nh += ret;
-        nm += !ret;
-
-    }
-
+  for (int64_t s : search_data) {
+    int ret = um1.exists(s);
+    nh += ret;
+    nm += !ret;
+  }
 }
 
 void test_dsmap_erase() {
-    auto& um1 = *ptr_um1;
+  auto& um1 = *ptr_um1;
 
-    for (int i = 0; i < NUM_ELEMENTS; ++i) {
-        int64_t s = erase_data[i];
-        ne += um1.erase(s);
-    }
-
-
+  for (int i = 0; i < NUM_ELEMENTS; ++i) {
+    int64_t s = erase_data[i];
+    ne += um1.erase(s);
+  }
 }
-
 
 void test_dsmap_insert_find() {
-    auto& um1 = *ptr_um1;
+  auto& um1 = *ptr_um1;
 
-    for (int i = 0; i < NUM_ELEMENTS; ++i) {
-        int64_t v = ins_data[i];
-        ni += um1.insert(v);
-        int64_t s = search_data[i];
+  for (int i = 0; i < NUM_ELEMENTS; ++i) {
+    int64_t v = ins_data[i];
+    ni += um1.insert(v);
+    int64_t s = search_data[i];
 
-      bool ret = um1.exists(s);
-        nh += ret;
-        nm += !ret;
-    }
+    bool ret = um1.exists(s);
+    nh += ret;
+    nm += !ret;
+  }
 }
-
-
 
 absl::flat_hash_set<int64_t>* ptrFshi;
 
 void test_fhs_insert() {
+  auto& fshi = *ptrFshi;
 
-    auto& fshi = *ptrFshi;
+  for (int i = 0; i < NUM_ELEMENTS; ++i) {
+    int64_t v = ins_data[i];
 
-    for (int i = 0; i < NUM_ELEMENTS; ++i) {
-        int64_t v = ins_data[i];
-
-        fshi.insert(v);
-        ++ni;
-    }
-
-
+    fshi.insert(v);
+    ++ni;
+  }
 }
 
 void test_fhs_search() {
-    auto& fshi = *ptrFshi;
+  auto& fshi = *ptrFshi;
 
-    for (int64_t s : search_data) {
-
-
-        bool ret = fshi.find(s) != fshi.end();
-        nh += ret;
-        nm += !ret;
-    }
-
-
+  for (int64_t s : search_data) {
+    bool ret = fshi.find(s) != fshi.end();
+    nh += ret;
+    nm += !ret;
+  }
 }
-
 
 void test_fhs_erase() {
-    auto& fshi = *ptrFshi;
+  auto& fshi = *ptrFshi;
 
-    for (int i = 0; i < NUM_ELEMENTS; ++i) {
-        int64_t s = erase_data[i];
-        
-        ne += fshi.erase(s);
-    }
+  for (int i = 0; i < NUM_ELEMENTS; ++i) {
+    int64_t s = erase_data[i];
 
-
+    ne += fshi.erase(s);
+  }
 }
 
+int main(int argc, const char* argv[]) {
+  if (argc < 2) return -1;
 
+  ptr_um1 = new ul_set_t();
+  ptrFshi = new absl::flat_hash_set<int64_t>();
+  auto& fshi = *ptrFshi;
 
-int main(int argc, const char * argv[]) {
+  genData();
+  std::cout << "Generated data\n";
 
-    if (argc < 2) return -1;
+  int opt = atoi(argv[1]);
 
-    ptr_um1 = new ul_map_t();
-    ptrFshi = new absl::flat_hash_set<int64_t>();
-    auto& fshi = *ptrFshi;
+  if (opt == 1) {
+    // us.reserve(NUM_ELEMENTS);
+    const int64_t t1 = rdtsc();
+    test_stdmap_insert();
+    const int64_t t2 = rdtsc();
 
-    genData();
-    std::cout << "Generated data\n";
-
-    int opt = atoi(argv[1]);
-
-    if (opt == 1) {
-        //us.reserve(NUM_ELEMENTS);
-        const int64_t t1 = rdtsc();
-        test_stdmap_insert();
-        const int64_t t2 = rdtsc();
-
-        test_stdmap_search();
-        const int64_t t3 = rdtsc();
-        std::cout << "test_stdmap insert:" << ni << "\n";
-        std::cout << "test_stdmap find:" << nh << "," << nm << "\n";
-        std::cout << "insert cycles " << t2 - t1 << ", search cycles " << t3 - t2 << "\n";
-        std::cout << "bucket count " << us.bucket_count() << "\n";
-    } else if (opt == 2) {
-        //us.reserve(NUM_ELEMENTS);
-        const int64_t t1 = rdtsc();
-        test_stdmap_insert_search();
-        const int64_t t2 = rdtsc();
-        std::cout << "test_stdmap insert_find:" << ni << "," << nh << "," << nm << "\n";
-        std::cout << "insert + search cycles " << t2 - t1 << "\n";
-        std::cout << "bucket count " << us.bucket_count() << "\n";
-    } else if (opt == 3) {
-        //us.reserve(NUM_ELEMENTS);
-        const int64_t t1 = rdtsc();
-        test_stdmap_insert();
-        const int64_t t2 = rdtsc();
-        test_stdmap_search();
-        const int64_t t3 = rdtsc();
-        test_stdmap_erase();
-        const int64_t t4 = rdtsc();
-        std::cout << "test_stdmap insert:" << ni << "\n";
-        std::cout << "test_stdmap find:" << nh << "," << nm << "\n";
-        std::cout << "test_stdmap erase:" << ne << "\n";
-        std::cout << "insert cycles " << t2 - t1 << ", search cycles " << t3 - t2 << ", erase cycles " << t4 - t3
-                  << "\n";
-    } else if (opt == 4) {
-        const int64_t t1 = rdtsc();
-        test_dsmap_insert();
-        const int64_t t2 = rdtsc();
-        test_dsmap_find();
-        const int64_t t3 = rdtsc();
-        std::cout << "test_dsmap insert:" << ni << "\n";
-        std::cout << "test_dsmap find:" << nh << "," << nm << "\n";
-        std::cout << "insert cycles " << t2 - t1 << ", search cycles " << t3 - t2 << "\n";
-    } else if (opt == 5) {
-        const int64_t t1 = rdtsc();
-        test_dsmap_insert_find();
-        const int64_t t2 = rdtsc();
-        std::cout << "test_dsmap insert_find:" << ni << "," << nh << "," << nm << "\n";
-        std::cout << "insert + search cycles " << t2 - t1 << "\n";
-    } 
-    else if (opt == 6) {
-        const int64_t t1 = rdtsc();
-        test_dsmap_insert();
-        const int64_t t2 = rdtsc();
-        test_dsmap_find();
-        const int64_t t3 = rdtsc();
-        test_dsmap_erase();
-        const int64_t t4 = rdtsc();
-        std::cout << "test_dsmap insert:" << ni << "\n";
-        std::cout << "test_dsmap find:" << nh << "," << nm << "\n";
-        std::cout << "test_dsmap erase:" << ne << "\n";
-        std::cout << "insert cycles " << t2 - t1 << ", search cycles " << t3 - t2 << ", erase cycles " << t4 - t3
-                  << "\n";
-    }
-    else if (opt == 7) {
-        const int64_t t1 = rdtsc();
-        test_fhs_insert();
-        const int64_t t2 = rdtsc();
-        test_fhs_search();
-        const int64_t t3 = rdtsc();
-        test_fhs_erase();
-        const int64_t t4 = rdtsc();
-        std::cout << "test_fhs insert:" << ni << "\n";
-        std::cout << "test_fhs find:" << nh << "," << nm << "\n";
-        std::cout << "test_fhs erase:" << ne << "\n";
-        std::cout << "insert cycles " << t2 - t1 << ", search cycles " << t3 - t2 << ", erase cycles " << t4 - t3
-                  << "\n";
-        std::cout << "test_fhs bucket count:" << fshi.bucket_count() << "\n";
-    }
+    test_stdmap_search();
+    const int64_t t3 = rdtsc();
+    std::cout << "test_stdmap insert:" << ni << "\n";
+    std::cout << "test_stdmap find:" << nh << "," << nm << "\n";
+    std::cout << "insert cycles " << t2 - t1 << ", search cycles " << t3 - t2
+              << "\n";
+    std::cout << "bucket count " << us.bucket_count() << "\n";
+  } else if (opt == 2) {
+    // us.reserve(NUM_ELEMENTS);
+    const int64_t t1 = rdtsc();
+    test_stdmap_insert_search();
+    const int64_t t2 = rdtsc();
+    std::cout << "test_stdmap insert_find:" << ni << "," << nh << "," << nm
+              << "\n";
+    std::cout << "insert + search cycles " << t2 - t1 << "\n";
+    std::cout << "bucket count " << us.bucket_count() << "\n";
+  } else if (opt == 3) {
+    // us.reserve(NUM_ELEMENTS);
+    const int64_t t1 = rdtsc();
+    test_stdmap_insert();
+    const int64_t t2 = rdtsc();
+    test_stdmap_search();
+    const int64_t t3 = rdtsc();
+    test_stdmap_erase();
+    const int64_t t4 = rdtsc();
+    std::cout << "test_stdmap insert:" << ni << "\n";
+    std::cout << "test_stdmap find:" << nh << "," << nm << "\n";
+    std::cout << "test_stdmap erase:" << ne << "\n";
+    std::cout << "insert cycles " << t2 - t1 << ", search cycles " << t3 - t2
+              << ", erase cycles " << t4 - t3 << "\n";
+  } else if (opt == 4) {
+    const int64_t t1 = rdtsc();
+    test_dsmap_insert();
+    const int64_t t2 = rdtsc();
+    test_dsmap_find();
+    const int64_t t3 = rdtsc();
+    std::cout << "test_dsmap insert:" << ni << "\n";
+    std::cout << "test_dsmap find:" << nh << "," << nm << "\n";
+    std::cout << "insert cycles " << t2 - t1 << ", search cycles " << t3 - t2
+              << "\n";
+  } else if (opt == 5) {
+    const int64_t t1 = rdtsc();
+    test_dsmap_insert_find();
+    const int64_t t2 = rdtsc();
+    std::cout << "test_dsmap insert_find:" << ni << "," << nh << "," << nm
+              << "\n";
+    std::cout << "insert + search cycles " << t2 - t1 << "\n";
+  } else if (opt == 6) {
+    const int64_t t1 = rdtsc();
+    test_dsmap_insert();
+    const int64_t t2 = rdtsc();
+    test_dsmap_find();
+    const int64_t t3 = rdtsc();
+    test_dsmap_erase();
+    const int64_t t4 = rdtsc();
+    std::cout << "test_dsmap insert:" << ni << "\n";
+    std::cout << "test_dsmap find:" << nh << "," << nm << "\n";
+    std::cout << "test_dsmap erase:" << ne << "\n";
+    std::cout << "insert cycles " << t2 - t1 << ", search cycles " << t3 - t2
+              << ", erase cycles " << t4 - t3 << "\n";
+  } else if (opt == 7) {
+    const int64_t t1 = rdtsc();
+    test_fhs_insert();
+    const int64_t t2 = rdtsc();
+    test_fhs_search();
+    const int64_t t3 = rdtsc();
+    test_fhs_erase();
+    const int64_t t4 = rdtsc();
+    std::cout << "test_fhs insert:" << ni << "\n";
+    std::cout << "test_fhs find:" << nh << "," << nm << "\n";
+    std::cout << "test_fhs erase:" << ne << "\n";
+    std::cout << "insert cycles " << t2 - t1 << ", search cycles " << t3 - t2
+              << ", erase cycles " << t4 - t3 << "\n";
+    std::cout << "test_fhs bucket count:" << fshi.bucket_count() << "\n";
+  }
 }
-
---
+++ src/unordered_set_bit.hh
@@ -0,0 +1,243 @@
+/**
+MIT License
+
+Copyright (c) 2025 Pawan Chawla (pawan.s.chawla@outlook.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+*/
+
+#ifndef UNORDERED_SET_BIT_IMPL_HH
+#define UNORDERED_SET_BIT_IMPL_HH
+
+#include <cstdint>
+
+/* usage: typedef ds::ul_set<64,22> ul_set; // you may choose to use more or
+ * less bits, more will increase memory usage. ul_set::insert(int64_t)
+ * ul_set::erase(int64_t)
+ * ul_set::exists(int64_t)
+ * */
+
+namespace ds {
+namespace internal {
+static constexpr int64_t sentinel_value = -1;
+
+struct data_t {
+  data_t() : value(0) {}
+
+  data_t(int v) : value(v) {}
+
+  int64_t value;
+};
+
+struct ul_prefix_node_chunk_t {
+  ul_prefix_node_chunk_t() : ptrNextChunk(nullptr) {
+    for (uint8_t i = 0; i < ul_prefix_node_chunk_t::MAX_COUNT; ++i) {
+      numbers[i] = sentinel_value;
+    }
+  }
+
+  static constexpr uint8_t MAX_COUNT = 4;  // this can be tuned as well
+  ul_prefix_node_chunk_t *ptrNextChunk;
+  int64_t numbers[MAX_COUNT];
+};
+
+static_assert(sizeof(ul_prefix_node_chunk_t) <=
+              std::hardware_destructive_interference_size);
+
+struct alignas(std::hardware_destructive_interference_size) ul_prefix_node_t {
+  ul_prefix_node_t() : totalCount(0) {}
+
+  bool addNum(int64_t n) {
+    for (ul_prefix_node_chunk_t *ptrChunk = &headChunk; ptrChunk;
+         ptrChunk = ptrChunk->ptrNextChunk) {
+      auto &numbers = ptrChunk->numbers;
+
+      for (int i = 0; i < ul_prefix_node_chunk_t::MAX_COUNT; ++i) {
+        if (numbers[i] == sentinel_value) {
+          numbers[i] = n;
+          ++totalCount;
+          return true;
+        } else if (numbers[i] == n) {
+          return false;
+        }
+      }
+
+      if (!ptrChunk->ptrNextChunk) {
+        auto *newChunk = new ul_prefix_node_chunk_t();
+        ptrChunk->ptrNextChunk = newChunk;
+        newChunk->numbers[0] = n;
+        ++totalCount;
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  ~ul_prefix_node_t() {
+    auto *ptrHeapChunk = headChunk.ptrNextChunk;
+
+    while (ptrHeapChunk) {
+      auto t = ptrHeapChunk->ptrNextChunk;
+      delete (ptrHeapChunk);
+      ptrHeapChunk = t;
+    }
+  }
+
+  int32_t totalCount;
+  ul_prefix_node_chunk_t headChunk;
+};
+
+static constexpr int NUM_BITS_PER_BYTE = 8;
+
+template <int maxBits, int numBitsPerNode>
+struct ul_layout_t {
+  static_assert(maxBits <= 64);
+
+  ul_layout_t() {}
+
+  const inline ul_prefix_node_t *getNode(int8_t idxUnit,
+                                         int32_t unitBytes) const {
+    return nodes[idxUnit * numNodesPerUnit + unitBytes];
+  }
+
+  inline ul_prefix_node_t &getNode(int8_t idxUnit, int32_t unitBytes) {
+    auto &n = nodes[idxUnit * numNodesPerUnit + unitBytes];
+
+    return n;
+  }
+
+  inline const ul_prefix_node_t &getNode(int32_t offset) const {
+    return nodes[offset];
+  }
+
+  inline ul_prefix_node_t &getNode(int32_t offset) { return nodes[offset]; }
+
+  static constexpr int64_t numNodesPerUnit = 1LL << (numBitsPerNode);
+  static constexpr int64_t numBitsPerUnit = numBitsPerNode;
+  static constexpr int64_t unitBitMask = (1LL << (numBitsPerNode)) - 1;
+  static constexpr int64_t numUnits = maxBits / numBitsPerNode;
+  static constexpr int64_t partialUnitBytes = 1LL << (maxBits % numBitsPerNode);
+  static constexpr int64_t numNodes = numUnits * numNodesPerUnit;
+  ul_prefix_node_t nodes[numNodes];
+};
+}  // namespace internal
+
+template <int maxBits = 64, int numBitsPerNode = 24>
+struct ul_set {
+  typedef internal::ul_layout_t<maxBits, numBitsPerNode> node_t;
+  static constexpr int MAX_BITS = maxBits;
+  static constexpr int NUM_BITS_PER_NODE = numBitsPerNode;
+
+  ul_set() : buckets() {}
+
+  bool exists(const int64_t num) {
+    const auto &node = buckets.getNode(num & node_t::unitBitMask);
+    const internal::ul_prefix_node_t *ptrMinNodeList = &node;
+
+    for (uint8_t i = 0; i < internal::ul_prefix_node_chunk_t::MAX_COUNT; ++i) {
+      if (node.headChunk.numbers[i] == num) {
+        return true;
+      }
+    }
+
+    if (!node.headChunk.ptrNextChunk) return false;
+
+    const internal::ul_prefix_node_chunk_t *searchChunk =
+        ptrMinNodeList->headChunk.ptrNextChunk;
+
+    int64_t num2 = num >> node_t::numBitsPerUnit;
+
+    for (int unit_idx = 1, offset = node_t::numNodesPerUnit;
+         unit_idx < node_t::numUnits;
+         ++unit_idx, offset += node_t::numNodesPerUnit) {
+      const int32_t digit = num2 & node_t::unitBitMask;
+      const internal::ul_prefix_node_t &nn = buckets.getNode(offset + digit);
+
+      if (!nn.totalCount)
+        return false;
+      else if (ptrMinNodeList->totalCount > nn.totalCount) {
+        ptrMinNodeList = &nn;
+        searchChunk = &nn.headChunk;
+      }
+
+      num2 >>= node_t::numBitsPerUnit;
+    }
+
+    for (const internal::ul_prefix_node_chunk_t *ptrChunk = searchChunk;
+         ptrChunk; ptrChunk = ptrChunk->ptrNextChunk) {
+      for (int i = 0; i < internal::ul_prefix_node_chunk_t::MAX_COUNT; ++i) {
+        if (ptrChunk->numbers[i] == num) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  int erase(int64_t num) {
+    int64_t num2 = num;
+
+    for (int unit_idx = 0, offset = 0; unit_idx < node_t::numUnits;
+         ++unit_idx, offset += node_t::numNodesPerUnit) {
+      internal::ul_prefix_node_t &nn =
+          buckets.getNode(offset + (num2 & node_t::unitBitMask));
+
+      for (internal::ul_prefix_node_chunk_t *ptrChunk = &nn.headChunk; ptrChunk;
+           ptrChunk = ptrChunk->ptrNextChunk) {
+        auto &nns = ptrChunk->numbers;
+        for (int i = 0; i < internal::ul_prefix_node_chunk_t::MAX_COUNT; ++i) {
+          if (nns[i] == num) {
+            nns[i] = internal::sentinel_value;
+            num2 >>= node_t::numBitsPerUnit;
+            goto next;
+          }
+        }
+      }
+      return 0;
+    next:
+      --nn.totalCount;
+    }
+  done:
+    return 1;
+  }
+
+  bool insert(int64_t num) {
+    int64_t num2 = num;
+
+    for (int unit_idx = 0; unit_idx < buckets.numUnits; ++unit_idx) {
+      const int digit = num2 & buckets.unitBitMask;
+
+      if (internal::ul_prefix_node_t &nn = buckets.getNode(unit_idx, digit);
+          !nn.addNum(num)) {
+        return false;
+      }
+      num2 >>= buckets.numBitsPerUnit;
+    }
+    return true;
+  }
+
+  internal::ul_layout_t<maxBits, numBitsPerNode> buckets;
+};
+}  // namespace ds
+
+#endif  // UNORDERED_SET_BIT_IMPL_HH
